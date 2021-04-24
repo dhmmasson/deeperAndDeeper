3404dc83f774f1c9de487ac69b594d6485564aec
dhmmasson <dhm.masson+github@gmail.com>
2021-04-24 08:50:57 +0200

    [Feature] Obstacle Spawning
    SImple spawning with a coroutine. Spawn accelerate over time
    
    TODO: Change despawning
    TODO: Accelerate also obstacle

--- Assets/Scripts/Falling.cs
+++ Assets/Scripts/Falling.cs
@@ -15,11 +15,23 @@ public class Falling : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        float dHeight = transform.position.y - previousHeight; 
+        float dHeight = transform.position.y - previousHeight;
+        GameObject objectToDestroy = null ;
+        obstacles = GameObject.FindGameObjectsWithTag("falling");
         foreach (GameObject obstacle in obstacles)
         {           
-            obstacle.transform.Translate(new Vector3(0, dHeight, 0)); 
+            obstacle.transform.Translate(new Vector3(0, dHeight, 0));
+            if (obstacle.transform.position.y < 0)
+            {
+                
+                objectToDestroy = obstacle; 
+            }
+        }
+        if (objectToDestroy != null)
+        {
+            GameObject.Destroy(objectToDestroy); ;            
         }
         previousHeight = transform.position.y;
+
     }
 }
--- Assets/Scripts/spawningPlane.cs
+++ Assets/Scripts/spawningPlane.cs
@@ -3,9 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class spawningPlane : MonoBehaviour
-{    
+{
+    public GameObject obstacle;
+    public float interval;
+    public float timeScale = 0.99f; 
     void Start()
     {
-            
+        spawn();
+        
+        StartCoroutine("SpawnObstacle");
+    }
+
+    private IEnumerator SpawnObstacle()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(interval); // wait two minutes
+            spawn();
+            interval *= timeScale; 
+        }
+       
+    }
+    private void spawn()
+    {
+        Instantiate(obstacle, new Vector3(0, 50, 0), Quaternion.Euler(0, Random.Range(-90, 90), 0), this.transform);
     }
 }
