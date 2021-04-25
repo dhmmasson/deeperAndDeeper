0b2578a0bf170954f0bd8a8e532b1f32528d4d5b
dhmmasson <dhm.masson+github@gmail.com>
2021-04-25 23:04:10 +0200

    [Compo] First release of the compo
    Expect some adjustment

--- Assets/Scripts/GameLogic.cs
+++ Assets/Scripts/GameLogic.cs
@@ -11,11 +11,15 @@ public class GameLogic : MonoBehaviour
     public GameObject fallingMarker;
     public TMP_Text Depth;
     public GameObject GameUi;
+    public TMP_Text DeepestUi;
     [Header("Shader")]
     public Autostereogram shader;
     [Header("Obstacle Spawning")]
     public GameObject obstacle;
-    private Transform spawnPoint; 
+    private Transform spawnPoint;
+
+    public GameObject[] FlavorObject;
+
     public float interval;
     public float timeScale = 0.99f;
     [Header("Info")]
@@ -26,8 +30,8 @@ public class GameLogic : MonoBehaviour
     private int currentDepth = 0;
     private float savedTimeScale = 0;
 
-    private bool menuEntered = false; 
-    
+    private bool menuEntered = false;
+    private int deepest = 0; 
 
 
     private void Start()
@@ -57,18 +61,10 @@ public class GameLogic : MonoBehaviour
         if (falling)
         {
             currentDepth = ((int)-fallingMarker.transform.position.y);
-            Depth.text = "Current Depth : " + currentDepth;
+            Depth.text = "Current Depth : " + currentDepth +"m" ;
         }
-        else
-        {
-            Depth.text = "Game over : " + currentDepth;
-        }
-        //Reset Game 
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            StartGame();
-        }
-        if (Input.GetKeyDown(KeyCode.Escape))
+        
+        if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.P))
         {
             PauseGame();
             menuEntered = true;
@@ -105,8 +101,8 @@ public class GameLogic : MonoBehaviour
         started = true;
         falling = true; 
         fallingMarker.transform.position = new Vector3(5, 0, 0);
-        savedTimeScale = 1;
-
+        
+        savedTimeScale = 1;        
         ContinueGame();
     }
     void PauseGame()
@@ -155,17 +151,26 @@ public class GameLogic : MonoBehaviour
     private void spawn()
     {
         Instantiate(obstacle, new Vector3(0, 50, 0), Quaternion.Euler(0, Random.Range(-90, 90), 0), spawnPoint);
+        //for( int i = 0; i < Random.Range(2,5); i++ )
+        //{            
+        //    Instantiate(FlavorObject[Random.Range(0, FlavorObject.Length)], new Vector3(Random.Range(-10,10), Random.Range(10, 40), Random.Range(-10, 10)), Quaternion.Euler(0, Random.Range(-90, 90), 0), spawnPoint);
+        //}
     }
     //----------- Collision Handling -----------//
     void OnCollisionEnter(Collision collision)
     {
         falling = false;
-        shader.autoStereogram.color = new Color(1, 0.8f, 0.8f, 1);
+        shader.autoStereogram.color = new Color(0.64f, 0.36f, 0.364f, 1);
+        if (currentDepth > deepest) deepest = currentDepth;
+        DeepestUi.SetText("You went " + deepest + "m deep!\nCan you go deeper and deeper?");
         StartCoroutine("Splash");
+        
     }
     private IEnumerator Splash()
     {
         yield return new WaitForSeconds(0.1f); // wait two minutes
         shader.autoStereogram.color = Color.black ;
+        Time.timeScale = 1;
+        StartGame();
     }
 }
